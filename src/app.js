0d576e75e59b94f013e673624c53a66a0d3eb8c6
DIEGO FLORES ALCANTAR <diegoedvflores03@gmail.com>
2023-03-21 15:33:58 -0700

    newChanges

--- src/app.js
+++ src/app.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const app = express()
-const routerProducts = require('./routers/routerProducts')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+
+const handlebars = require('express-handlebars')
+const productManager = require('./productManager')
+const routerProducts = require('./routers/routerProducts')(io)
 const routerCarts = require('./routers/routerCarts')
 
+
+app.engine('handlebars', handlebars.engine())
+app.set('views',__dirname + '/views')
 
 app.use(express.json())
 app.use('/api/products', routerProducts)
 app.use('/api/carts', routerCarts)
 
 
-const port = 8080
-app.listen(port, ()=>{
-  console.log(`server running at port ${port}`)
+io.on('connection', (socket) => {
+  console.log('usuario conectado')
+
+  socket.on('productos', (data) => {
+    console.log('Productos resividos del servidor')
+    socket.emit('productos', productManager.getProducts())
+  })
+
+  socket.on('disconnect', () => {
+    console.log('usuario desconectado')
+  })
+})
+
+const puerto = 5550
+http.listen(puerto, ()=>{
+  console.log(`server running at port ${puerto}`)
 })
--- src/routers/routerProducts.js
+++ src/routers/routerProducts.js
@@ -1,40 +1,81 @@
 const { Router } = require('express')
+const { string } = require('joi')
 const productManager = require('../productManager')
 const router = Router()
 
 
-// Permite crar un producto desde el body, siempre y cuando sea un objeto con todas sus propiedaddes adecuadas
+
+
+/**
+ * {HANDLEBARS}
+ */
+
+module.exports = (io) => {
+  // Permite crar un producto desde el body, siempre y cuando sea un objeto con todas sus propiedaddes adecuadas
 router.post('/', (req, res) => {
 
-  const {product} = req.body
+  const {nombre, price, descripcion, stock, code, category, img} = req.body
+  const status = req.body.status === 'on'
 
-  try {
-    productManager.addProduct(product)
-    res.status(201).send({message: 'producto creado'})
-  } catch (error) {
-    console.error(error)
-    res.status(400).send({error:`error al crear el producto, verifica que sea un objeto y cuente con las claves y valores correctos`})
-  }
+  console.log(nombre, price, descripcion, stock, code, category, img, status)
+
+  // const product = {
+  //   "nombre":nombre,
+  //   "descripcion":descripcion,
+  //   "price":price,
+  //   "stock":stock,
+  //   "code":code,
+  //   "category":category,
+  //   "status":status,
+  //   "img":img
+  // }
+  // console.log(product)
+  // try {
+  //   productManager.addProduct(product)
+  //   res.status(201).send({message: 'producto creado'})
+
+  //   io.emit('productos', productManager.getProducts())
+  // } catch (error) {
+  //   console.error(error)
+  //   res.status(400).send({error:`error al crear el producto, verifica que sea un objeto y cuente con las claves y valores correctos`})
+  // }
 })
 
-// Muestra todos los productos existentes
+// Muestra todos los productos existentes en el dom con handlebars
 router.get('/', (req, res) => {
   const productsLim = parseInt(req.query.limit)
   try {
     const productos = productManager.getProducts(productsLim)
-    res.status(200).send({message: productos})
+    res.status(200).render('home.handlebars', {productos: productos})
   } catch (error) {
     console.error(error)
     res.status(500).send({error:`error al cargar los productos`})
   }
 })
 
+
+//Muestra los productos en tiempo real
+router.get('/realTimeProducts', (req, res) => {
+  const productsLimite = parseInt(req.query.limit)
+  try {
+    const productos = productManager.getProducts(productsLimite)
+    // Envía los productos a los clientes conectados
+    io.emit('productos', productos)
+
+    res.status(200).render('realTimeProducts.handlebars', {productos: productos})
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({error:`error al cargar los productos`})
+  }
+})
+
+
 // Muestra un producto en particular
 router.get('/:pid', (req, res) => {
   const productId = parseInt(req.params.pid)
   try {
     const product = productManager.getProductById(productId)
-    res.status(200).send({message: product})
+    res.status(200).send({product: product})
   } catch (error) {
     console.error(error)
     res.status(404).send({message: `producto ${productId} no encontrado`})
@@ -47,7 +88,8 @@ router.patch('/:pid', (req, res) => {
   const updates = req.body
   try {
     const updateProduct = productManager.updateProduct(productId, updates)
-    res.status(200).send({message: updateProduct})
+    res.status(200).send({updateProduct: updateProduct})
+    io.emit('productos', productManager.getProducts())
   } catch (error) {
     console.error(error)
     res.status(400).send({message: `error al actualizar ru producto, verifica los valores enviados por body`})
@@ -61,10 +103,13 @@ router.delete('/:pid', (req, res) => {
   try {
     productManager.deleteProduct(productId)
     res.status(200).send({message: `Producto con id ${productId} ha sido eliminado`})
+    io.emit('productos', productManager.getProducts())
   } catch (error) {
     console.error(error)
     res.status(500).send({error: `Error al eliminar el producto con id ${productId}`})
   }
 })
 
-module.exports = router
+return router
+}
+
